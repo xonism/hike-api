3d1aed6e3fbd0ad196a68d2dd24f949943220611
xonism <gabriele.minc@protonmail.com>
2023-07-18 23:00:25 +0300

    Add HikeApplicationTests

---
+++ src/test/java/com/api/hikes/HikeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.api.hikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
