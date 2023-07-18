82a065df1bdd67ecd2488b142b045c8337925664
xonism <gabriele.minc@protonmail.com>
2023-07-18 22:56:24 +0300

    Add ItemRepository

---
+++ src/main/java/com/api/hikes/repositories/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.api.hikes.repositories;
+
+import com.api.hikes.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findBySeason(String season);
+}
