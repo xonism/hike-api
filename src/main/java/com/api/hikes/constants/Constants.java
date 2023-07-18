1feb59a8635b8d37a82f2faa0eb476c828424b01
xonism <gabriele.minc@protonmail.com>
2023-07-18 22:55:39 +0300

    Add Constants

---
+++ src/main/java/com/api/hikes/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.api.hikes.constants;
+
+public class Constants {
+
+    public static final int KILOMETERS_PER_WATER_LITRE = 10;
+    public static final int KILOMETERS_BEFORE_SLEEP = 50;
+    public static final int BASE_DAILY_REQUIRED_CALORIES = 2_000;
+    public static final int CALORIES_BURNED_PER_KILOMETER = 50;
+
+    private Constants() {
+
+    }
+}
