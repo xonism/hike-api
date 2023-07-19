33ffa920b01a0ea5544b477aa102f9cbd88c152e
xonism <gabriele.minc@protonmail.com>
2023-07-19 22:30:53 +0300

    Add getById & getAll tests to ItemServiceTests

--- src/test/java/com/api/hikes/services/ItemServiceTests.java
+++ src/test/java/com/api/hikes/services/ItemServiceTests.java
@@ -1,7 +1,8 @@
 package com.api.hikes.services;
 
-import com.api.hikes.models.Item;
 import com.api.hikes.exceptions.InvalidSeasonException;
+import com.api.hikes.exceptions.NotFoundByIdException;
+import com.api.hikes.models.Item;
 import com.api.hikes.records.ItemRequest;
 import com.api.hikes.repositories.ItemRepository;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowableOfType;
@@ -27,6 +29,60 @@ class ItemServiceTests {
 
     @InjectMocks
     private ItemService underTest;
+
+    @Test
+    void givenValidId_whenGetItemById_thenItemIsReturned() {
+        long id = 1L;
+
+        Item expectedResult = Item.builder().id(id).name("Item").season("Season").build();
+
+        when(itemRepository.findById(id))
+                .thenReturn(Optional.ofNullable(expectedResult));
+
+        Item actualResult = underTest.getById(id);
+
+        assertThat(actualResult).isNotNull().usingRecursiveComparison().isEqualTo(expectedResult);
+
+        verify(itemRepository).findById(id);
+        verifyNoMoreInteractions(itemRepository);
+    }
+
+    @Test
+    void givenInvalidId_whenGetItemById_thenExceptionIsThrown() {
+        long id = 1L;
+
+        when(itemRepository.findById(id)).thenReturn(Optional.empty());
+
+        String actualMessage =
+                catchThrowableOfType(() -> underTest.getById(id), NotFoundByIdException.class)
+                        .getMessage();
+
+        String expectedMessage = new NotFoundByIdException("Item").getMessage();
+
+        assertThat(actualMessage).isNotNull().isEqualTo(expectedMessage);
+
+        verify(itemRepository).findById(id);
+        verifyNoMoreInteractions(itemRepository);
+    }
+
+    @Test
+    void givenItems_whenGetAll_ThenAllItemsAreReturned() {
+        List<Item> expectedResult = List.of(
+                Item.builder().id(1).name("Item 1").season("Season").build(),
+                Item.builder().id(2).name("Item 2").season("Season").build(),
+                Item.builder().id(3).name("Item 3").season("Season").build()
+        );
+
+        when(itemRepository.findAllByOrderByIdAsc())
+                .thenReturn(expectedResult);
+
+        List<Item> actualResult = underTest.getAll();
+
+        assertThat(actualResult).isNotNull().usingRecursiveComparison().isEqualTo(expectedResult);
+
+        verify(itemRepository).findAllByOrderByIdAsc();
+        verifyNoMoreInteractions(itemRepository);
+    }
 
     @Test
     void givenValidItemRequest_whenCreateItem_thenItemIsCreated() {
@@ -60,7 +116,7 @@ class ItemServiceTests {
     }
 
     @Test
-    void givenValidId_whenDeleteItem_thenItemIsDeleted() {
+    void givenId_whenDeleteItem_thenItemIsDeleted() {
         long id = 1L;
 
         underTest.delete(id);
