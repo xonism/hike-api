95325fe882dcc873febb471d93d925b7ca575640
xonism <gabriele.minc@protonmail.com>
2023-07-19 13:08:32 +0300

    Add additional annotations to Item

--- src/main/java/com/api/hikes/entities/Item.java
+++ src/main/java/com/api/hikes/entities/Item.java
@@ -7,11 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "item")
 public class Item {
