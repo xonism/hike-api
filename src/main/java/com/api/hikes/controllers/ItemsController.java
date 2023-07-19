541733ed4d6a5f21e423d0471756b2f4d1d30585
xonism <gabriele.minc@protonmail.com>
2023-07-19 21:52:58 +0300

    Add getItem & getAllItems to ItemsController

--- src/main/java/com/api/hikes/controllers/ItemsController.java
+++ src/main/java/com/api/hikes/controllers/ItemsController.java
@@ -1,17 +1,20 @@
 package com.api.hikes.controllers;
 
-import com.api.hikes.entities.Item;
+import com.api.hikes.models.Item;
 import com.api.hikes.records.ItemRequest;
 import com.api.hikes.services.ItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/items")
@@ -23,13 +26,29 @@ public class ItemsController {
         this.itemService = itemService;
     }
 
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Item> getItem(@PathVariable long id) {
+        return new ResponseEntity<>(
+                itemService.getById(id),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<Item>> getAllItems() {
+        return new ResponseEntity<>(
+                itemService.getAll(),
+                HttpStatus.CREATED);
+    }
+
     @PostMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Item> createItem(@RequestBody ItemRequest itemRequest) {
         return new ResponseEntity<>(
                 itemService.create(itemRequest),
-                HttpStatus.OK);
+                HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/{id}")
