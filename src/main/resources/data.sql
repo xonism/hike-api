25dea30b6855bc4a2c7a1bc5f432866739967dc4
xonism <gabriele.minc@protonmail.com>
2023-07-19 13:08:09 +0300

    Reduce data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,13 @@
-INSERT INTO item (id, name, season) VALUES (1, 'Cap', 'summer');
-INSERT INTO item (id, name, season) VALUES (2, 'Insect repellent', 'summer');
-INSERT INTO item (id, name, season) VALUES (9, 'Sunscreen', 'summer');
-
-INSERT INTO item (id, name, season) VALUES (3, 'Rain boots', 'autumn');
-INSERT INTO item (id, name, season) VALUES (4, 'Headlamp', 'autumn');
-INSERT INTO item (id, name, season) VALUES (10, 'Raincoat', 'autumn');
-
-INSERT INTO item (id, name, season) VALUES (5, 'Scarf', 'winter');
-INSERT INTO item (id, name, season) VALUES (6, 'Gloves', 'winter');
-INSERT INTO item (id, name, season) VALUES (11, 'Earmuffs', 'winter');
-
-INSERT INTO item (id, name, season) VALUES (7, 'Coat', 'spring');
-INSERT INTO item (id, name, season) VALUES (8, 'Umbrella', 'spring');
-INSERT INTO item (id, name, season) VALUES (12, 'Waterproof boots', 'spring');
+INSERT INTO item (name, season) VALUES
+    ('cap', 'summer'),
+    ('insect repellent', 'summer'),
+    ('sunscreen', 'summer'),
+    ('rain boots', 'autumn'),
+    ('headlamp', 'autumn'),
+    ('raincoat', 'autumn'),
+    ('scarf', 'winter'),
+    ('gloves', 'winter'),
+    ('earmuffs', 'winter'),
+    ('coat', 'spring'),
+    ('umbrella', 'spring'),
+    ('waterproof boots', 'spring');
